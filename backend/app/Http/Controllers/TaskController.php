6ec850ed7b5362468856c0cb0fac22e536d28d02
Yuma NAGANO <y.nagano@secure-cycle.co.jp>
2021-12-24 15:46:29 +0900

    コンストラクタの変数名をtaskに修正

--- backend/app/Http/Controllers/TaskController.php
+++ backend/app/Http/Controllers/TaskController.php
@@ -10,38 +10,38 @@ use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
-    protected $tasks;
+    protected $task;
 
-    public function __construct(Task $tasks)
+    public function __construct(Task $task)
     {
-        $this->tasks = $tasks;
+        $this->task = $task;
     }
 
     public function index(Request $request)
     {
-        $task = $this->tasks;
+        $tasks = $this->task;
         //TODO: whereInメソッドで短くかけるかもしれない
         if (!is_null($request->completed) && !is_null($request->notCompleted)) {
         } elseif (!is_null($request->completed)) {
-            $task = $task->where('is_completed', '=', true);
+            $tasks = $tasks->where('is_completed', '=', true);
         } elseif (!is_null($request->notCompleted) || (is_null($request->completed) && is_null($request->notCompleted))) {
-            $task = $task->where('is_completed', '=', false);
+            $tasks = $tasks->where('is_completed', '=', false);
         }
 
         if (!is_null($request->startDeadline)) {
-            $task = $task->where('deadline', '>=', $request->startDeadline);
+            $tasks = $tasks->where('deadline', '>=', $request->startDeadline);
         }
 
         if (!is_null($request->endDeadline)) {
-            $task = $task->where('deadline', '<=', $request->endDeadline);
+            $tasks = $tasks->where('deadline', '<=', $request->endDeadline);
         }
 
         if (!is_null($request->freeWord)) {
-            $task = $task->where('name', 'like', "%{$request->freeWord}%");
+            $tasks = $tasks->where('name', 'like', "%{$request->freeWord}%");
         }
 
         return view('tasks', [
-            'tasks' => $task->sortable()->paginate(10),
+            'tasks' => $tasks->sortable()->paginate(10),
             'current_time' => new DateTime()
         ]);
     }
